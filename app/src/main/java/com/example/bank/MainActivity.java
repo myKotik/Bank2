f641d21e4fe37eb0b7591a0509e089358bea73cd
Katrina <Lebedeva.kate6518@gmail.com>
2021-09-29 18:57:41 +0200

    mur

--- app/src/main/java/com/example/bank/MainActivity.java
+++ app/src/main/java/com/example/bank/MainActivity.java
@@ -3,22 +3,14 @@ package com.example.bank;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-    //private Document doc;
+
     TextView data;
 
-    //private Thread secThread;
-    //private Runnable runnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,26 +23,6 @@ public class MainActivity extends AppCompatActivity {
        String d = simpleDataFormat.format(c.getTime());
 //        data.setText(d);
 
-       // init();
 
     }
-    /*private void init(){
-        runnable = new Runnable() {
-            @Override
-            public void run() {
-                getWeb();
-            }
-        };
-        secThread = new Thread(runnable);
-        secThread.start();
-    }*/
-
-    /*private void getWeb(){
-        try {
-            doc = Jsoup.connect("www.cbr.ru").get();
-            Log.d("MyLog", "Title" + doc.text());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }*/
 }
