376e0716c7962c818c3dc2dee61800f894191e49
Katrina <Lebedeva.kate6518@gmail.com>
2021-09-29 12:59:55 +0200

    mur

---
+++ app/src/main/java/com/example/bank/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.bank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView data;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Дата 
+        //data = findViewById(R.id.data);
+       // Calendar c = Calendar.getInstance();
+       // SimpleDateFormat simpleDataFormat = new SimpleDateFormat("dd-MM-yy");
+       // String d = simpleDataFormat.format(c.getTime());
+       // data.setText(d);
+    }
+}
